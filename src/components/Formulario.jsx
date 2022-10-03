8b0d49f7c76df3c78aabac97653ec0c23194d676
francSCh <salaschfr@gmail.com>
2022-10-03 15:51:30 -0500

    Primer Commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+        <form>
+            <input type="submit" value="Cotizar"/>
+        </form>
+    )
+}
+
+export default Formulario
