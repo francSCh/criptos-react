c47ac17d51ca83fe5a37c8d44d18bc29c4d0463b
francSCh <salaschfr@gmail.com>
2022-10-03 19:56:13 -0500

    Consultamos la API de criptomonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect } from 'react'
+import styled from '@emotion/styled'
+
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+/* Styleds Components */
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
 
 const Formulario = () => {
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCriptos = resultado.Data.map(cripto => {
+                console.log(cripto.CoinInfo.Name);
+                console.log(cripto.CoinInfo.FullName);
+            });
+        }
+
+        consultarAPI();
+    },[]);
+
   return (
         <form>
-            <input type="submit" value="Cotizar"/>
+            <SelectMonedas />
+
+            <InputSubmit type="submit" value="Cotizar"/>
         </form>
     )
 }
